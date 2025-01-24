3e4b3d4f753f8ab380820fb0ca75d41253a4a036
Cole-nnn <laicola930527@gmail.com>
2025-01-24 12:10:03 +0800

    Update app.py

--- rag/app.py
+++ rag/app.py
@@ -1,43 +1,50 @@
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, render_template, request, jsonify
 import os
-from langchain_openai import OpenAIEmbeddings
+from langchain_community.embeddings import OpenAIEmbeddings
 from langchain_community.vectorstores import Chroma
 from langchain.chains.question_answering import load_qa_chain
 from langchain_community.callbacks import get_openai_callback
-from langchain_openai import ChatOpenAI
+from langchain_community.chat_models import ChatOpenAI
 from opencc import OpenCC
 import openai
-from openai import OpenAI
 
-openai.api_key=os.getenv('OPENAI_API_KEY')
+# 設定 OpenAI API 金鑰
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='rag/templates', static_folder='rag/static')
+
+# 聊天歷史記錄
+chat_history = []
+
 @app.route('/')
-def index():
+def home():
     return render_template('index.html')
 
-@app.route('/get_response', methods=['POST'])
+@app.route("/get_response", methods=["POST"])
 def get_response():
-    user_input = request.form.get('user_input')
+    user_input = request.json.get('question')
     if not user_input:
         return jsonify({'error': 'No user input provided'})
 
     embeddings = OpenAIEmbeddings()
-    db = Chroma(persist_directory="./db/temp/", embedding_function=embeddings)
+    db = Chroma(persist_directory="./rag/db/temp/", embedding_function=embeddings)
     docs = db.similarity_search(user_input)
     
     llm = ChatOpenAI(
-        model_name="gpt-4o",
+        model_name="gpt-4",
         temperature=0.5
     )
     chain = load_qa_chain(llm, chain_type="stuff")
+    
     with get_openai_callback() as cb:
         response = chain.invoke({"input_documents": docs, "question": user_input}, return_only_outputs=True)
-
+    
     cc = OpenCC('s2t')
     answer = cc.convert(response['output_text'])
 
+    chat_history.append({'user': user_input, 'assistant': answer})
+
     return jsonify({'response': answer})
 
-if __name__ == '__main__':
-  app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
